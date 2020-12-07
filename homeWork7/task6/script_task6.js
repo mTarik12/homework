6a9f35d36ca34f799b485f6d1823123c05571923
mTarik12 <72448312+mTarik12@users.noreply.github.com>
2020-12-07 20:46:07 +0200

    Delete script_task6.js

--- homeWork7/task6/script_task6.js
+++
@@ -1,7 +0,0 @@
-tellSizing = () => {
-    const myWidth = window.innerWidth;
-    const myHeight = window.innerHeight;
-
-    document.write(`Width: ${myWidth}, Height: ${myHeight}`);
-}
-tellSizing();
